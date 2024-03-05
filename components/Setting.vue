56d62bdf3a41a1fe322a727e8f9b38c09d96d957
zaa555 <zaa07012078@gmail.com>
2024-03-05 01:45:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/Setting.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {useLocalStorage} from "@vueuse/core";
+
+const safeReply = useLocalStorage('safeReply', true)
+const openaiKey = useLocalStorage('openaiKey', '')
+const SDXL_steps = useLocalStorage('SDXL_steps', 20)
+</script>
+
+<template>
+  <div class="p-4 flex flex-col space-y-3">
+    <div class="font-bold text-lg">
+      {{ $t('setting') }}
+    </div>
+    <div class="flex items-baseline">
+      {{ $t('block_sensitive_reply') }} (GeminiPro)
+      <UToggle v-model="safeReply" class="ml-auto" disabled/>
+    </div>
+    <div class="flex items-end">
+      <div class="flex flex-col">
+        OPENAI_API_KEY
+        <span class="text-xs text-gray-500">{{ $t('use_own_key') }}</span>
+      </div>
+      <UInput v-model="openaiKey" class="ml-auto"/>
+    </div>
+    <div>
+      <div class="flex">
+        {{ $t('img_gen_steps') }}
+        <span class="ml-auto">{{ SDXL_steps }}</span>
+      </div>
+      <URange min="1" max="20" v-model="SDXL_steps" class="w-full"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    devtools: {enabled: false},
+    modules: ['@nuxt/ui', '@nuxtjs/i18n'],
+    css: ['~/assets/css/style.css'],
+    devServer: {
+        port: 3001,
+    },
+    routeRules: {
+        '/': {
+            prerender: true,
+        }
+    },
+    app: {
+        head: {
+            title: 'CF AI Web',
+            meta: [
+                {
+                    name: 'keywords',
+                    content: 'CF AI Web, AI, Cloudflare Workers, ChatGPT, GeminiPro, Google Generative AI'
+                },
+                {
+                    name: 'description',
+                    content: 'Integrated web platform supporting GeminiPro/Cloudflare Workers AI/ChatGPT by Jazee6'
+                }
+            ]
+        }
+    },
+    i18n: {
+        vueI18n: './i18n.config.ts',
+        strategy: 'no_prefix',
+        defaultLocale: 'zh',
+    }
+    // nitro: {
+    //     vercel: {
+    //         regions: ["cle1", "iad1", "pdx1", "sfo1", "sin1", "syd1", "hnd1", "kix1"]
+    //     }
+    // }
+})
